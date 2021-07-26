1acabfa6da3f52bad05ea23a51c1449cfbc3a076
rayme persad <raymepersad@gmail.com>
2021-07-25 21:42:55 -0400

    fourth commit added sort and search java classes

---
+++ src/main/java/ucf/assignments/search.java
@@ -0,0 +1,22 @@
+package ucf.assignments;
+
+public class search {
+
+        private String searchSerialNumber;
+        private String searchName;
+
+
+        public String getSearchSerialNumber() {
+            return searchSerialNumber;
+        }
+
+        public String getSearchName() {
+            return searchName;
+        }
+
+        public search(String searchSerialNumber, String searchName) {
+            super();
+            this.searchSerialNumber = searchSerialNumber;
+            this.searchName = searchName;
+        }
+    }
---
+++ src/main/java/ucf/assignments/sort.java
@@ -0,0 +1,24 @@
+package ucf.assignments;
+
+public class sort {
+        private String sortValue;
+        private String sortSerialNumber;
+
+
+
+        public String getSortValue() {
+            return sortValue;
+        }
+
+        public String getSortSerialNumber() {
+            return sortSerialNumber;
+        }
+
+
+        public sort(String sortValue, String sortSerialNumber){
+            super();
+            this.sortValue = sortValue;
+            this.sortSerialNumber = sortSerialNumber;
+
+        }
+    }
